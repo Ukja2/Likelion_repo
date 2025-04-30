b82ae7ca510f7ebd4f8fcb165b2f550d39fa96b8
Ukja2 <jaewooklee739@gmail.com>
2025-04-30 20:48:57 +0900

    React 라우터를 활용한 다중 페이지 생성

--- ReactPractice/my-react-app/src/App.js
+++ ReactPractice/my-react-app/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Routes, Route , useNavigate} from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 
-function App() {
 
+const App = () => {
 
-  // function greeting(name){
+  const navigate = useNavigate()
+
+    // function greeting(name){
   //   console.log(`안녕하세요! ${name}님`);
   // }
 
@@ -20,32 +23,44 @@ function App() {
 
 
   
-  const handleClick = () => {
-    alert("안녕!");
-  };
+  // const handleClick = () => {
+  //   alert("안녕!");
+  // };
 
   
-  const Button = (props) => {
-    return (
-      <button className="myButton" onClick={props.onClick}>
-        {props.text}
-      </button>
-    );
-  };
-
-  
+  // const Button = (props) => {
+  //   return (
+  //     <button className="myButton" onClick={props.onClick}>
+  //       {props.text}
+  //     </button>
+  //   );
+  // };
 
 
-  
+
+  // Link의 To는 Route의 Path를 지정하는 역할을 한다.
+  // Link는 Route의 Path와 연결되어 있다.
   return (
-    <div>
-      <Button text="클릭하세요" onClick={handleClick} />
-      <Button text="저장하기" onClick={() => alert("저장되었습니다!")} />
-      <Home />
-      <About/>
-      <Contact />
+    <div className="app">
+      {/* 네비게이션 메뉴 */}
+      <nav className="navbar">
+        <ul>
+          <li><button onClick={() => navigate('/')}>홈</button></li>
+          <li><button onClick={() => navigate('/about')}>소개</button></li>
+          <li><button onClick={() => navigate('/contact')}>연락처</button></li>
+        </ul>
+      </nav>
+
+      {/* 라우트 설정 */}
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
